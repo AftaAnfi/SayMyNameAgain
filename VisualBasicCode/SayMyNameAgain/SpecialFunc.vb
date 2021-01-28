fcb4387a0efcf369c10d12d1037bfbeeeb6bf822
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-28 08:13:22 -0700

    Added Special Func (notes) in project

---
+++ VisualBasicCode/SayMyNameAgain/SpecialFunc.vb
@@ -0,0 +1,9 @@
+﻿Option Strict On
+Option Explicit On
+Module SpecialFunc
+
+    Sub Main()
+
+    End Sub
+
+End Module
--- VisualBasicCode/SayMyNameAgain/SubFunctionExamples.vb
+++ VisualBasicCode/SayMyNameAgain/SubFunctionExamples.vb
@@ -4,9 +4,14 @@ Option Strict On
 Module SubFunctionExamples
 
     Sub Main()
-        Console.WriteLine("I'm in sub main.")
-        MyOtherSub()
-        DoMath()
+        'Console.WriteLine("I'm in sub main.")
+        'MyOtherSub()
+
+        'DoMath(3, 1)
+        'DoMath(8, 0)
+        'Console.WriteLine(firstFunction())
+        'Console.WriteLine(MyCoolFunc("String"))
+
         Console.ReadKey(False)
 
     End Sub
@@ -17,8 +22,33 @@ Module SubFunctionExamples
 
     End Sub
 
-    Sub DoMath()
-        Console.WriteLine(2 + 2)
+    'pass data to a sub/function with arguments
+    Sub DoMath(someNumber As Integer, secondNumber As Integer)
+        someNumber *= 2
+        Console.WriteLine(someNumber + secondNumber)
     End Sub
+
+    Function firstFunction() As String
+        'mycoolfool <- Tim's function
+
+
+        'Return "henlo"
+        Return 0.ToString()
+    End Function
+
+    Function MyCoolFunc(noteToSelf As String) As String
+        Dim temp As String
+        temp = ($"hello, {noteToSelf}")
+        Return temp
+    End Function
+
+
+
+
+
+
+
+
+
 
 End Module
