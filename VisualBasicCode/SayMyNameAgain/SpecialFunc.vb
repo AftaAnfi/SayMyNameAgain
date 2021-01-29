5f819b482969095d2aa7fd9cd632f5795d38fa74
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-29 08:11:30 -0700

    Added Functions StoreMessages

--- VisualBasicCode/SayMyNameAgain/SpecialFunc.vb
+++ VisualBasicCode/SayMyNameAgain/SpecialFunc.vb
@@ -4,6 +4,29 @@ Module SpecialFunc
 
     Sub Main()
 
+        promptUser("Hetlo")
+
+        For i = 0 To 5
+            storeMessages("hello")
+        Next
+
+        promptUser(storeMessages(""))
+
+        Console.Read()
+
     End Sub
+
+    Sub promptUser(message As String)
+        Console.WriteLine(message)
+    End Sub
+
+
+    Function storeMessages(message As String) As String
+        'static doesn't change scope but lifetime is foreverrrrrrrrrrrrrr (as long as program is open)
+        Static storedMessages As String
+        storedMessages &= message
+        Return storedMessages
+    End Function
+
 
 End Module
