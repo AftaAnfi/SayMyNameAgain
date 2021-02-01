9449623b2ef72372cbf37e34a11802acbbacd0fe
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-31 17:17:04 -0700

    clean code

--- VisualBasicCode/SayMyNameAgain/SayMyNameAgain.vb
+++ VisualBasicCode/SayMyNameAgain/SayMyNameAgain.vb
@@ -12,34 +12,19 @@ Module SayMyNameAgain
         Select Case readInputString
             Case "Joe"
                 Console.WriteLine($"Hello {readInputString}!")
-
             Case "Emily"
-
                 Console.WriteLine($"Hello {readInputString}!")
             Case "Aftanom"
                 Console.WriteLine($"Awh yes, the programmer {readInputString} has arrived.")
             Case Else
                 Console.WriteLine("Hello!")
                 Console.WriteLine("Have a good day!")
-
         End Select
 
         Console.WriteLine("Press any key to continue...")
         Console.ReadKey(False)
 
-        'if it's joe or emily
-        'do this
-
-        'if not
-        'do this (not name)
-
-
-
-
 
     End Sub
-
-
-
 
 End Module
