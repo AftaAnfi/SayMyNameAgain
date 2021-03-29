f38c5d6ea3419e6f81b72c9c2455ffea004f1495
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-03-29 08:49:09 -0600

    Added Informative comments and change the scope of readInputString

--- VisualBasicCode/SayMyNameAgain/SayMyNameAgain.vb
+++ VisualBasicCode/SayMyNameAgain/SayMyNameAgain.vb
@@ -9,13 +9,14 @@ Option Explicit On
 
 Module SayMyNameAgain
 
-    Dim readInputString As String
-
     Sub Main()
-        Console.WriteLine("What is your name?")
+        Dim readInputString As String
 
+        'prompt user and gather user input 
+        Console.WriteLine("What is your name?")
         readInputString = Console.ReadLine()
 
+        'select case based on the name given from user
         Select Case readInputString
             Case "Joe"
                 Console.WriteLine($"Hello {readInputString}!")
@@ -24,13 +25,14 @@ Module SayMyNameAgain
             Case "Aftanom"
                 Console.WriteLine($"Awh yes, the programmer {readInputString} has arrived.")
             Case Else
+                'if not Joe Emily or Aftanom, display a simple hello
                 Console.WriteLine("Hello!")
                 Console.WriteLine("Have a good day!")
         End Select
 
+        'Prompt user on exitting the program
         Console.WriteLine("Press any key to continue...")
         Console.ReadKey(False)
-
 
     End Sub
 
